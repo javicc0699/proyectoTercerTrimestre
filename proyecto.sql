7592001b7b6ac3325a2c0691b93c6f4bc2a7395c
javicc0699 <148794102+javicc0699@users.noreply.github.com>
2024-05-19 21:04:31 +0200

    Añadido Modelo Fisico y Relacional

---
+++ proyecto.sql
@@ -0,0 +1,65 @@
+DROP DATABASE IF EXISTS proyecto;
+CREATE DATABASE proyecto CHARSET utf8mb4;
+USE proyecto;
+
+CREATE TABLE coche (
+
+    Matricula VARCHAR(7) PRIMARY KEY,
+    Marca VARCHAR(45) NOT NULL,
+    Modelo VARCHAR(45) NOT NULL,
+    Anyo VARCHAR(4) NOT NULL,
+    Caballos DOUBLE NOT NULL,
+    Manejo DOUBLE NOT NULL
+
+);
+
+CREATE TABLE Pieza (
+
+    Pieza VARCHAR(15) PRIMARY KEY,
+    Caballosprop DOUBLE,
+    Pmanejo DOUBLE
+
+);
+
+CREATE TABLE Circuito (
+
+    idCircuito INT PRIMARY KEY,
+    Curvas INT NOT NULL,
+    Ubicacion VARCHAR(45),
+    Dificultad ENUM('Baja', 'Media', 'Alta') NOT NULL
+
+);
+
+CREATE TABLE Modificar (
+
+    Matricula VARCHAR(7),
+    Pieza VARCHAR(15),
+    Marca VARCHAR(45),
+    Calidad ENUM('Fabrica' , 'Urbana' , 'Profesional' , 'Competicion'),
+    PRIMARY KEY (Matricula, Pieza),
+    FOREIGN KEY (Matricula) REFERENCES Coche(Matricula),
+    FOREIGN KEY (Pieza) REFERENCES Pieza(Pieza)
+);
+
+CREATE TABLE Competicion (
+    idCompeticion INT PRIMARY KEY AUTO_INCREMENT,
+    idCircuito INT,
+    Matricula VARCHAR(7),
+    Vueltas INT NOT NULL,
+    Fecha DATE NOT NULL,
+    Meteorologia ENUM('Soleado', 'Lluvioso', 'Niebla') NOT NULL,
+    Hora ENUM('Amanecer', 'Dia', 'Noche') NOT NULL,
+    FOREIGN KEY (idCircuito) REFERENCES Circuito(idCircuito),
+    FOREIGN KEY (Matricula) REFERENCES Coche(Matricula)
+);
+
+INSERT INTO coche (Matricula, Marca, Modelo, Anyo, Caballos, Manejo)
+VALUES ('ABC1234', 'Nissan', 'GTR', '2020', 500, 70);
+INSERT INTO Pieza (Pieza, Caballosprop, Pmanejo)
+VALUES ('Motor', 140, 0);
+INSERT INTO Circuito (idCircuito, Curvas, Ubicacion, Dificultad)
+VALUES (1, 154, 'Alemania', 'Alta');
+
+
+
+
