a7e01e8f19acb359641c84e259cb5249681ce7d9
javicc0699 <148794102+javicc0699@users.noreply.github.com>
2024-05-23 19:54:59 +0200

    Update Modelo FIsico.sql

--- Modelo FIsico.sql
+++ Modelo FIsico.sql
@@ -2,43 +2,39 @@ DROP DATABASE IF EXISTS proyecto;
 CREATE DATABASE proyecto CHARSET utf8mb4;
 USE proyecto;
 
-CREATE TABLE coche (
-
+CREATE TABLE Coche (
     Matricula VARCHAR(7) PRIMARY KEY,
     Marca VARCHAR(45) NOT NULL,
     Modelo VARCHAR(45) NOT NULL,
     Anyo VARCHAR(4) NOT NULL,
     Caballos DOUBLE NOT NULL,
     Manejo DOUBLE NOT NULL
-
 );
 
-CREATE TABLE Pieza (
-
+CREATE TABLE TipoPieza (
     Pieza VARCHAR(15) PRIMARY KEY,
-    Caballosprop DOUBLE,
-    Pmanejo DOUBLE
-
+    CaballosPorc DOUBLE,
+    ManejoPorc DOUBLE
 );
 
+-- Crear tabla Circuito
 CREATE TABLE Circuito (
-
     idCircuito INT PRIMARY KEY,
     Curvas INT NOT NULL,
     Ubicacion VARCHAR(45),
-    Dificultad ENUM('Baja', 'Media', 'Alta') NOT NULL
-
+    Dificultad VARCHAR(15) NOT NULL
 );
 
-CREATE TABLE Modificar (
 
+CREATE TABLE PiezasCoche (
     Matricula VARCHAR(7),
     Pieza VARCHAR(15),
-    Marca VARCHAR(45),
-    Calidad ENUM('Fabrica' , 'Urbana' , 'Profesional' , 'Competicion'),
+    Calidad VARCHAR(45),
+    CaballosPieza DOUBLE,
+    ManejoPieza DOUBLE,
     PRIMARY KEY (Matricula, Pieza),
     FOREIGN KEY (Matricula) REFERENCES Coche(Matricula),
-    FOREIGN KEY (Pieza) REFERENCES Pieza(Pieza)
+    FOREIGN KEY (Pieza) REFERENCES TipoPieza(Pieza)
 );
 
 CREATE TABLE Competicion (
@@ -46,27 +42,25 @@ CREATE TABLE Competicion (
     idCircuito INT,
     Vueltas INT NOT NULL,
     Fecha DATE NOT NULL,
-    Meteorologia ENUM('Soleado', 'Lluvioso', 'Niebla') NOT NULL,
-    Hora ENUM('Amanecer', 'Dia', 'Noche') NOT NULL,
+    Meteorologia VARCHAR(45) NOT NULL,
+    Hora VARCHAR(45) NOT NULL,
     FOREIGN KEY (idCircuito) REFERENCES Circuito(idCircuito)
-    
 );
 
 CREATE TABLE Participantes (
-	idCompeticion INT PRIMARY KEY auto_increment,
-    idMatricula VARCHAR(7),
+    idCompeticion INT,
+    Matricula VARCHAR(7),
+    PRIMARY KEY (idCompeticion, Matricula),
     FOREIGN KEY (idCompeticion) REFERENCES Competicion(idCompeticion),
     FOREIGN KEY (Matricula) REFERENCES Coche(Matricula)
-
 );
 
-INSERT INTO coche (Matricula, Marca, Modelo, Anyo, Caballos, Manejo)
+
+INSERT INTO Coche (Matricula, Marca, Modelo, Anyo, Caballos, Manejo)
 VALUES ('ABC1234', 'Nissan', 'GTR', '2020', 500, 70);
-INSERT INTO Pieza (Pieza, Caballosprop, Pmanejo)
+
+INSERT INTO TipoPieza (Pieza, CaballosPorc, ManejoPorc)
 VALUES ('Motor', 140, 0);
+
 INSERT INTO Circuito (idCircuito, Curvas, Ubicacion, Dificultad)
 VALUES (1, 154, 'Alemania', 'Alta');
-
-
-
-
