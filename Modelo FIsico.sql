922c7e229ff19ebb2903983924778c6e3085a568
javicc0699 <148794102+javicc0699@users.noreply.github.com>
2024-05-29 01:59:19 +0200

    Añadido m fisico y m relacional actualizado

--- Modelo FIsico.sql
+++ Modelo FIsico.sql
@@ -13,7 +13,7 @@ CREATE TABLE Coche (
 
 CREATE TABLE Pieza (
     Codigo INT PRIMARY KEY,
-    Pieza VARCHAR(15) NOT NULL,
+    Pieza VARCHAR(45) NOT NULL,
     Calidad VARCHAR(45),
     CaballosProp DOUBLE,
     ManejoProp DOUBLE
@@ -27,11 +27,13 @@ CREATE TABLE Circuito (
 );
 
 CREATE TABLE PiezaCoche (
-	Codigo INT PRIMARY KEY,
+    Codigo INT,
     Matricula VARCHAR(7),
+    PRIMARY KEY (Codigo, Matricula),
     FOREIGN KEY (Matricula) REFERENCES Coche(Matricula),
     FOREIGN KEY (Codigo) REFERENCES Pieza(Codigo)
 );
+
 
 CREATE TABLE Competicion (
     idCompeticion INT PRIMARY KEY AUTO_INCREMENT,
@@ -51,6 +53,25 @@ CREATE TABLE Participantes (
     FOREIGN KEY (Matricula) REFERENCES Coche(Matricula)
 );
 
+
+INSERT INTO Pieza (Codigo, Pieza, Calidad, CaballosProp, ManejoProp) VALUES (1, 'Motor Urbano', 'Urbana', 30, 0);
+INSERT INTO Pieza (Codigo, Pieza, Calidad, CaballosProp, ManejoProp) VALUES (2, 'Motor Profesional', 'Profesional', 90, 0);
+INSERT INTO Pieza (Codigo, Pieza, Calidad, CaballosProp, ManejoProp) VALUES (3, 'Motor Competición', 'Competición', 130, 0);
+
+INSERT INTO Pieza (Codigo, Pieza, Calidad, CaballosProp, ManejoProp) VALUES (4, 'Escape 1', 'Urbana', 12, 0);
+INSERT INTO Pieza (Codigo, Pieza, Calidad, CaballosProp, ManejoProp) VALUES (5, 'Escape 2', 'Profesional', 35, 0);
+INSERT INTO Pieza (Codigo, Pieza, Calidad, CaballosProp, ManejoProp) VALUES (6, 'Escape 3', 'Competición', 43, 0);
+
+INSERT INTO Pieza (Codigo, Pieza, Calidad, CaballosProp, ManejoProp) VALUES (7, 'Frenos 1', 'Urbana', 0, 4);
+INSERT INTO Pieza (Codigo, Pieza, Calidad, CaballosProp, ManejoProp) VALUES (8, 'Frenos 2', 'Profesional', 0, 9);
+INSERT INTO Pieza (Codigo, Pieza, Calidad, CaballosProp, ManejoProp) VALUES (9, 'Frenos 3', 'Competición', 0, 15);
+
+INSERT INTO Pieza (Codigo, Pieza, Calidad, CaballosProp, ManejoProp) VALUES (10, 'Neumaticos 1', 'Urbana', 0, 14);
+INSERT INTO Pieza (Codigo, Pieza, Calidad, CaballosProp, ManejoProp) VALUES (11, 'Neumaticos 2', 'Profesional', 0, 24);
+INSERT INTO Pieza (Codigo, Pieza, Calidad, CaballosProp, ManejoProp) VALUES (12, 'Neumaticos 3', 'Competición', 0, 29);
+
+INSERT INTO Coche (Matricula, Marca, Modelo, Anyo, Caballos, Manejo)
+VALUES ('1000ccc', 'Toyota', 'Corolla', '2020', 150, 8.5)
 
 
 
