d7fe37f2a23e39325dc8f36c08fcced2678598cc
javicc0699 <148794102+javicc0699@users.noreply.github.com>
2024-05-29 02:08:09 +0200

    Añadidos comentarios explicativos

--- VistasProyecto.sql
+++ VistasProyecto.sql
@@ -1,5 +1,7 @@
 -- Vistas
 
+-- Esta vista muestra los coches y las piezas que tiene instaladas y las estadisticas que le proporciona
+DROP VIEW IF EXISTS VistaCochePiezas;
 CREATE VIEW VistaCochePiezas AS
 SELECT Coche.Matricula, Coche.Marca, Coche.Modelo, Pieza.Pieza, Pieza.Calidad, Pieza.CaballosProp, Pieza.ManejoProp
 FROM Coche
@@ -7,7 +9,9 @@ JOIN PiezaCoche ON Coche.Matricula = PiezaCoche.Matricula
 JOIN Pieza ON PiezaCoche.Codigo = Pieza.Codigo;
 
 SELECT * FROM VistaCochePiezas;
-
+-- Una vista que muestra detalles de las competiciones, incluyendo el identificador de la competicion, 
+-- la ubicación y dificultad del circuito y el numero de coches participantes en cada competicion.
+DROP VIEW IF EXISTS VistaCompeticionDetalles;
 CREATE VIEW VistaCompeticionDetalles AS
 SELECT Competicion.idCompeticion, Circuito.Ubicacion, Circuito.Dificultad, COUNT(Participantes.Matricula) AS NumCoches
 FROM Competicion
